7cc21cbe54a875b205c933630c1d2370fd674b23
Srinikhila M <23wh1a05a1@bvrithyderabad.edu.in>
2025-05-15 10:48:21 +0530

    backend_linklet added

---
+++ backend/backend_linklet/src/main/java/com/linklet/backend/BackendLinkletApplication.java
@@ -0,0 +1,13 @@
+package com.linklet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLinkletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLinkletApplication.class, args);
+	}
+
+}
---
+++ backend/backend_linklet/src/test/java/com/linklet/backend/BackendLinkletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linklet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLinkletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
